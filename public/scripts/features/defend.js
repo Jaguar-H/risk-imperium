92aaa320839654ce30d310285485f28bce7f0809
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2026-03-31 18:36:20 +0530

    [#6] | Abstracts resolve_combat from defend.js to resolve_combat.js | [Bhargavi/Amisha]

--- public/scripts/features/defend.js
+++ public/scripts/features/defend.js
@@ -1,33 +1,5 @@
-import { combat, defend } from "../APIS.js";
-import { showNotification } from "../utilities.js";
-
-const updateTroopsInMap = (territoryId, troopsCount) => {
-  const territoryElement = document.querySelector(
-    `[data-territory-id="${territoryId}"]`,
-  );
-  const troopCountElement = territoryElement.querySelector(".troop-count");
-  troopCountElement.textContent = troopsCount;
-};
-
-const updateMap = (prevData, data) => {
-  updateTroopsInMap(prevData.attackerTerritoryId, data.attackerTroops);
-  updateTroopsInMap(prevData.defenderTerritoryId, data.defenderTroops);
-  showNotification(data.msg);
-};
-
-const handleCombat = async (prevData, _action, gameState) => {
-  const { action: newAction, data } = await combat(prevData);
-  const diceValues = [...data.attackerDice, ...data.defenderDice];
-  const dieElements = document.querySelectorAll(".die-slot");
-
-  dieElements.forEach((dice, index) => {
-    dice.classList.toggle(".dice-roll");
-    dice.textContent = diceValues[index];
-  });
-
-  gameState.state = newAction;
-  updateMap(prevData, data);
-};
+import { defend } from "../APIS.js";
+import { handleCombat } from "./resolve_combat.js";
 
 export const handleDefend = async (territory, gameState) => {
   const territoryId = Number(territory.dataset.territoryId);
